dd48ee2c625732ef6ff5c93b958d48228df27893
Havard Bjerke <havard.bjerke@aspentech.com>
2025-09-02 08:46:59 +0200

    Fix postgres DB creation

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_NAME" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE evolution;
-	GRANT ALL PRIVILEGES ON DATABASE evolution TO "$DB_USER";
+	GRANT ALL PRIVILEGES ON DATABASE evolution TO "$POSTGRES_USER";
 EOSQL
